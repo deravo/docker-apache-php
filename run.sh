f55eba7dff82331eac33dc09e9abf4a662126a07
deravo <deravo@163.com>
2016-04-13 15:13:38 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,18 +4,10 @@ if [ ! -f /.hiredis ]; then
     /hiredis.sh
 fi
 
-/etc/init.d/memcached start
-
-/etc/init.d/redis-server start
-
 php5enmod mcrypt
-
-# source /etc/apache2/envvars
-
-# tail -F /var/log/apache2/* &
-
+/etc/init.d/memcached start
+/etc/init.d/redis-server start
 apache2ctl start
 
-# exec apache2 -D FOREGROUND
 
 exec /usr/sbin/sshd -D
